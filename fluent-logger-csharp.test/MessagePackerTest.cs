dada7294277de2a0f2413492202f325958d07e2f
zoetrope <a.ikezoe@gmail.com>
2013-02-09 11:49:47 +0900

    pass to MessagePackerTest

--- fluent-logger-csharp.test/MessagePackerTest.cs
+++ fluent-logger-csharp.test/MessagePackerTest.cs
@@ -22,17 +22,16 @@ namespace Fluent.Test
         public void PackSimpleAnonymousObject()
         {
             var date = MessagePackConvert.ToDateTime(1360370224238);
-            var pack = _packer.MakePacket("test1", date, new { Message = "hoge" });
-            Utils.Unpack(pack).Is(@"[ ""test.test1"", 1360370224238, { ""Message"" : ""hoge"" } ]");
+            var pack = _packer.MakePacket("test1", date, new { Message = "hoge", Value = 1234 });
+            Utils.Unpack(pack).Is(@"[ ""test.test1"", 1360370224238, { ""Message"" : ""hoge"", ""Value"" : 1234 } ]");
         }
         [TestMethod]
         public void PackSimpleDictionary()
         {
             var date = MessagePackConvert.ToDateTime(1360370224238);
-            var msg = new Dictionary<string, object>() { { "Message","hoge"} };
+            var msg = new Dictionary<string, object>() { { "Message", "hoge" }, { "Value", 1234 } };
             var pack = _packer.MakePacket("test1", date, msg);
-
-            Utils.Unpack(pack).Is(@"[ ""test.test1"", 1360370224238, { ""Message"" : ""hoge"" } ]");
+            Utils.Unpack(pack).Is(@"[ ""test.test1"", 1360370224238, { ""Message"" : ""hoge"", ""Value"" : 1234 } ]");
         }
         [TestMethod]
         public void PackSimpleDynamicObject()
@@ -40,9 +39,94 @@ namespace Fluent.Test
             var date = MessagePackConvert.ToDateTime(1360370224238);
             dynamic msg = new ExpandoObject();
             msg.Message = "hoge";
+            msg.Value = 1234;
             var pack = _packer.MakePacket("test1", date, msg);
-            ((string)Utils.Unpack(pack)).Is(@"[ ""test.test1"", 1360370224238, { ""Message"" : ""hoge"" } ]");
+            ((string)Utils.Unpack(pack)).Is(@"[ ""test.test1"", 1360370224238, { ""Message"" : ""hoge"", ""Value"" : 1234 } ]");
         }
 
+        [TestMethod]
+        public void PackArray()
+        {
+            var date = MessagePackConvert.ToDateTime(1360370224238);
+            var pack = _packer.MakePacket("test1", date, new[] { "hoge", "1234" });
+            Utils.Unpack(pack).Is(@"[ ""test.test1"", 1360370224238, [ ""hoge"", ""1234"" ] ]");
+        }
+        [TestMethod]
+        public void PackList()
+        {
+            var date = MessagePackConvert.ToDateTime(1360370224238);
+            var pack = _packer.MakePacket("test1", date, new List<string> { "hoge", "1234" });
+            Utils.Unpack(pack).Is(@"[ ""test.test1"", 1360370224238, [ ""hoge"", ""1234"" ] ]");
+        }
+
+
+        [TestMethod]
+        public void PackStruct()
+        {
+
+            var date = MessagePackConvert.ToDateTime(1360370224238);
+            var s = new StructData { Name = "Struct", Value = 112233445566778899};
+            var pack = _packer.MakePacket("test1", date, s);
+            Utils.Unpack(pack).Is(@"[ ""test.test1"", 1360370224238, { ""Name"" : ""Struct"", ""Value"" : 112233445566778899 } ]");
+        }
+
+        public struct StructData
+        {
+            public string Name;
+            public long Value;
+        }
+
+        [TestMethod]
+        public void PackNestedData()
+        {
+
+            var date = MessagePackConvert.ToDateTime(1360370224238);
+
+            var parent = new Parent()
+            {
+                Name = "Parent",
+                Child1 = new Child { Name = "Child1", Value = 1 },
+                Child2 = new Child { Name = "Child2", Value = 2 },
+                Child3 = new Child { Name = "Child3", Value = 3 },
+                Child4 = new Child { Name = "Child4", Value = 4 },
+                Child5 = new Child { Name = "Child5", Value = 5 }
+            };
+            var pack = _packer.MakePacket("test1", date, parent);
+            Utils.Unpack(pack).Is(@"[ ""test.test1"", 1360370224238, { ""Name"" : ""Parent"", ""Child1"" : { ""Name"" : ""Child1"", ""Value"" : 1 }, ""Child2"" : { ""Name"" : ""Child2"", ""Value"" : 2 }, ""Child3"" : { ""Name"" : ""Child3"", ""Value"" : 3 }, ""Child4"" : { ""Name"" : ""Child4"", ""Value"" : 4 }, ""Child5"" : { ""Name"" : ""Child5"", ""Value"" : 5 } } ]");
+        }
+
+        public class Parent
+        {
+            public string Name { get; set; }
+            public Child Child1 { get; set; }
+            public Child Child2 { get; set; }
+            public Child Child3 { get; set; }
+            public Child Child4 { get; set; }
+            public Child Child5 { get; set; }
+            
+        }
+        public class Child
+        {
+            public string Name { get; set; }
+            public int Value { get; set; }
+        }
+
+        [TestMethod]
+        public void PackLoopRefType()
+        {
+            var date = MessagePackConvert.ToDateTime(1360370224238);
+            var child = new Node { Name = "Child" };
+            var node = new Node { Name = "Parent", Child = child };
+            child.Child = node;
+
+            var ex = AssertEx.Throws<InvalidOperationException>(() => _packer.MakePacket("test1", date, node));
+            ex.Message.Is("nest counter is over the maximum. counter = 11");
+        }
+
+        public class Node
+        {
+            public Node Child { get; set; }
+            public string Name { get; set; }
+        }
     }
 }
--- fluent-logger-csharp/MessagePacker.cs
+++ fluent-logger-csharp/MessagePacker.cs
@@ -1,4 +1,5 @@
 ﻿using MsgPack;
+using MsgPack.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Dynamic;
@@ -13,9 +14,14 @@ namespace Fluent
     internal class MessagePacker
     {
         private string _tag;
-        public MessagePacker(string tag)
+        private int _nestCount = 0;
+
+        public int MaxNestCount { get; set; }
+
+        public MessagePacker(string tag, int maxNestCount = 10)
         {
             _tag = tag;
+            MaxNestCount = maxNestCount;
         }
 
         public byte[] MakePacket(string label, DateTime timestamp, object obj)
@@ -33,6 +39,7 @@ namespace Fluent
             var xs = new List<MessagePackObject>();
             xs.Add(tag);
             xs.Add(MessagePackConvert.FromDateTime(timestamp));
+            _nestCount = 0;
             xs.Add(CreateTypedMessagePackObject(obj.GetType(), obj));
             var x = new MessagePackObject(xs);
 
@@ -43,41 +50,86 @@ namespace Fluent
         }
         private MessagePackObject CreateTypedMessagePackObject(Type type, object obj)
         {
-
-            if (type == typeof(bool)) return new MessagePackObject((bool)obj);
-            if (type == typeof(byte)) return new MessagePackObject((byte)obj);
-            if (type == typeof(byte[])) return new MessagePackObject((byte[])obj);
-            if (type == typeof(double)) return new MessagePackObject((double)obj);
-            if (type == typeof(float)) return new MessagePackObject((float)obj);
-            if (type == typeof(int)) return new MessagePackObject((int)obj);
-            if (type == typeof(long)) return new MessagePackObject((long)obj);
-            if (type == typeof(sbyte)) return new MessagePackObject((sbyte)obj);
-            if (type == typeof(short)) return new MessagePackObject((short)obj);
-            if (type == typeof(string)) return new MessagePackObject((string)obj);
-            if (type == typeof(uint)) return new MessagePackObject((uint)obj);
-            if (type == typeof(ulong)) return new MessagePackObject((ulong)obj);
-            if (type == typeof(ushort)) return new MessagePackObject((ushort)obj);
-
-            if (type.IsArray)
+            Console.WriteLine(_nestCount);
+            if (_nestCount > MaxNestCount)
             {
-                return new MessagePackObject((obj as object[]).Select(x => CreateTypedMessagePackObject(type.GetElementType(), x)).ToList());
+                throw new InvalidOperationException("nest counter is over the maximum. counter = " + _nestCount);
             }
 
-            if (obj is ExpandoObject || obj is IDictionary<string, object>)
+            _nestCount++;
+            using (var d = new DisposeWithAction(() => _nestCount--))
             {
-                var tmp = obj as IDictionary<string, object>;
-                var dict = tmp.ToDictionary(x => new MessagePackObject(x.Key), x => CreateTypedMessagePackObject(x.Value.GetType(), x.Value));
-                return new MessagePackObject(new MessagePackObjectDictionary(dict));
-            }
-            else
-            {
-                var dict = obj.GetType()
-                    .GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                    .ToDictionary(pi => new MessagePackObject(pi.Name), pi => CreateTypedMessagePackObject(pi.PropertyType, pi.GetValue(obj, null)));
-                return new MessagePackObject(new MessagePackObjectDictionary(dict));
-            }
+                if (obj == null)
+                {
+                    throw new ArgumentException("can't pack a null value. type = " + type.FullName, "type");
+                }
 
-            throw new ArgumentException("invalid type", "type");
+                if (type == typeof(bool)) return new MessagePackObject((bool)obj);
+                if (type == typeof(byte)) return new MessagePackObject((byte)obj);
+                if (type == typeof(byte[])) return new MessagePackObject((byte[])obj);
+                if (type == typeof(double)) return new MessagePackObject((double)obj);
+                if (type == typeof(float)) return new MessagePackObject((float)obj);
+                if (type == typeof(int)) return new MessagePackObject((int)obj);
+                if (type == typeof(long)) return new MessagePackObject((long)obj);
+                if (type == typeof(sbyte)) return new MessagePackObject((sbyte)obj);
+                if (type == typeof(short)) return new MessagePackObject((short)obj);
+                if (type == typeof(string)) return new MessagePackObject((string)obj);
+                if (type == typeof(uint)) return new MessagePackObject((uint)obj);
+                if (type == typeof(ulong)) return new MessagePackObject((ulong)obj);
+                if (type == typeof(ushort)) return new MessagePackObject((ushort)obj);
+                if (type == typeof(DateTime)) return new MessagePackObject(MessagePackConvert.FromDateTime((DateTime)obj));
+                if (type == typeof(DateTimeOffset)) return new MessagePackObject(MessagePackConvert.FromDateTimeOffset((DateTimeOffset)obj));
+
+                if (type.IsEnum) return new MessagePackObject(obj.ToString());
+
+                if (type.IsArray)
+                {
+                    return new MessagePackObject((obj as object[]).Select(x => CreateTypedMessagePackObject(type.GetElementType(), x)).ToList());
+                }
+
+                if (obj is ExpandoObject || obj is IDictionary<string, object>)
+                {
+                    var tmp = obj as IDictionary<string, object>;
+                    var dict = tmp.ToDictionary(x => new MessagePackObject(x.Key), x => CreateTypedMessagePackObject(x.Value.GetType(), x.Value));
+                    return new MessagePackObject(new MessagePackObjectDictionary(dict));
+                }
+                else if (type.IsValueType)
+                {
+                    var dict = obj.GetType()
+                        .GetFields(BindingFlags.Public | BindingFlags.Instance)
+                        .ToDictionary(fi => new MessagePackObject(fi.Name), fi => CreateTypedMessagePackObject(fi.FieldType, fi.GetValue(obj)));
+                    return new MessagePackObject(new MessagePackObjectDictionary(dict));
+                }
+                else if (type.GetInterfaces().Any(i => i == typeof(System.Collections.IEnumerable)))
+                {
+                    var list = new List<MessagePackObject>();
+                    foreach (var x in obj as System.Collections.IEnumerable)
+                    {
+                        list.Add(CreateTypedMessagePackObject(x.GetType(), x));
+                    }
+                    return new MessagePackObject(list);
+                }
+                else
+                {
+                    var dict = obj.GetType()
+                        .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                        .ToDictionary(pi => new MessagePackObject(pi.Name), pi => CreateTypedMessagePackObject(pi.PropertyType, pi.GetValue(obj, null)));
+                    return new MessagePackObject(new MessagePackObjectDictionary(dict));
+                }
+            }
+        }
+
+        public class DisposeWithAction : IDisposable
+        {
+            private Action _action;
+            public DisposeWithAction(Action action)
+            {
+                _action = action;
+            }
+            public void Dispose()
+            {
+                _action();
+            }
         }
     }
 }
